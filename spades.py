18aabf42e96c7d3fbe1abe2f3209a824b6e582b5
Yovel Key-Cohen <ykey-cohen@emeryweiner.org>
2024-07-29 23:19:03 -0500

    streamlined interrupts; ayel

--- spades.py
+++ spades.py
@@ -3,34 +3,57 @@ import csv, time, os
 ZERO_SCORE_ADD = 70
 
 TIMESTAMP = str(int(time.time()))
-UUID_N = 0
+
+def get_input(prompt: str):
+    inp = ''
+    while not inp:
+        inp = input(prompt).strip()
+    for i,interrupt in enumerate(Game.INTERRUPTS):
+        if inp == interrupt[0]:
+            return i
+    return inp
 
 class Team:
-    
-    def __init__(self, i, players=2):
+    def __init__(self, i, players=2, names=()):
         self.players = players
-        self.i = i+1
-        self.names = [input(f'Team {self.i} player {j+1}: ').title().strip() for j in range(players)]
+        self.i = i
+        if not names:
+            self.names = tuple(input(f'Team {self.i} player {j+1}: ').title().strip() for j in range(players))
+        else:
+            self.names = names
+            for j in range(players):
+                print(f'Team {self.i} player {j+1}:', self.names[j])
         
         self.score = 0
         self.bids = [0]*players
         self.tricks = [0]*players
 
-        self.stats = [] # bid, made, team_bust?, name
+        self.stats = [] # bid, made, team_bust?, name, uuid
 
     def playerlist(self):
         return '/'.join(self.names)
 
     def get_bids(self):
         for i,name in enumerate(self.names):
-            self.bids[i] = int(input(f'{name}\'s bid: '))
-    def get_tricks(self):
-        for i,name in enumerate(self.names):
-            self.tricks[i] = int(input(f'{name}\'s tricks: '))
+            inp = get_input(f'{name}\'s bid: ')
+            if type(inp) is int: return inp
+            self.bids[i] = int(inp)
 
-    def add_score(self):
-        global UUID_N, TIMESTAMP
-        UUID_N += 1
+    def get_tricks(self, round_tricks_list):
+        for i,name in enumerate(self.names):
+            if len(round_tricks_list) < 3:
+                inp = get_input(f'{name}\'s tricks: ')
+                if type(inp) is int: return inp
+                self.tricks[i] = int(inp)
+                round_tricks_list.append(int(inp))
+
+            else:
+                last = 13-sum(round_tricks_list)
+                self.tricks[i] = last
+                print(f'{name}\'s tricks:', last)
+
+    def add_score(self, round_i):
+        global TIMESTAMP
 
         bust = 1
         if sum(self.tricks) >= sum(self.bids):
@@ -38,7 +61,7 @@ class Team:
             bust = 0
 
         for trick, bid, name in zip(self.tricks, self.bids, self.names):
-            self.stats.append((bid, trick, bust, name, f'{TIMESTAMP}{UUID_N:03}'))
+            self.stats.append((bid, trick, bust, name, f'{TIMESTAMP}-{round_i:03}-{self.i}'))
             
             if bid == 0:
                 if trick == 0:
@@ -47,28 +70,42 @@ class Team:
                     self.score -= ZERO_SCORE_ADD
 
 class Game:
-    def __init__(self, teams=2, players_per_team=2):
-        self.teams = [Team(i, players=players_per_team) for i in range(teams)]
+    INTERRUPTS = [
+        ('CANCEL', 'Round canceled!'),
+        ('FORFEIT', 'Victory by forfeit!')
+    ]
+
+    AYEL_PLAYERS = (('Aviva', 'Yovel'), ('Ezra', 'Leia'))
+
+    def __init__(self, teams=2, players_per_team=2, ayel=False):
+        if ayel:
+            self.teams = [Team(i+1, players=players_per_team, names=Game.AYEL_PLAYERS[i]) for i in range(teams)]
+        else:
+            self.teams = [Team(i+1, players=players_per_team) for i in range(teams)]
         self.end = int(input('Score we are playing to: '))
+
+    def save(self):
+        os.makedirs('Games', exist_ok=True)
+        with open('Games/spades_'+TIMESTAMP+'.txt', 'w') as f:
+            writer = csv.writer(f, delimiter=' ')
+            for team in self.teams:
+                for stat in team.stats:
+                    writer.writerow(stat)
 
     def start(self):
         global TIMESTAMP
         
-        i = 0
+        i = 1
         while True:
-            i += 1
-            self.round(i)
-
-            if (winner := max(self.teams, key=(lambda t: t.score))).score > self.end:
-                print(f'\n{winner.playerlist()} wins!  {self.score()}')
-
-                os.makedirs('Games', exist_ok=True)
-                with open('Games/spades_'+TIMESTAMP+'.txt', 'w') as f:
-                    writer = csv.writer(f, delimiter=' ')
-                    for team in self.teams:
-                        for stat in team.stats:
-                            writer.writerow(stat)
+            interrupt = self.round(i)
+            if interrupt == 0:
+                continue
+            possible_winner = max(self.teams, key=(lambda t: t.score))
+            if interrupt == 1 or possible_winner.score > self.end:
+                print(f'\n{possible_winner.playerlist()} wins!  {self.score()}')
+                self.save()
                 return i
+            i += 1
     
     def score(self):
         scores = '-'.join(str(team.score) for team in self.teams)
@@ -77,15 +114,27 @@ class Game:
     def round(self, i):
         print(f"\nRound {i}  {self.score()}")
         print("="*30)
+
+        # collect bids
         for team in self.teams:
-            team.get_bids()
+            inter = team.get_bids()
+            if inter is not None:
+                print(Game.INTERRUPTS[inter][1])
+                return inter
+
         print("="*30)
+
+        # collect tricks
+        round_tricks_list = []
         for team in self.teams:
-            team.get_tricks()
-            team.add_score()
+            inter = team.get_tricks(round_tricks_list)
+            if inter is not None:
+                print(Game.INTERRUPTS[inter][1])
+                return inter
+            team.add_score(i)
 
         
 
 if __name__ == "__main__":
-    g = Game(teams=2)
+    g = Game(teams=2, ayel=True)
     g.start()
--- statreader.py
+++ statreader.py
@@ -8,7 +8,7 @@ records = defaultdict(list)
 
 MIN_DATE = dt(2024, 6, 3)
 MAX_DATE = dt.now()
-PRINT_EXTRA = False
+PRINT_EXTRA = True
 
 
 for fname in listdir('Games'):
@@ -57,24 +57,25 @@ for name, record in records.items():
     if PRINT_EXTRA: print('Core Stats:')
     print(f'\tAverage bid: {round(np.mean(bids), 2)} ± {round(np.std(bids), 2)}')
     print(f'\tAverage made: {round(np.mean(makes), 2)} ± {round(np.std(makes), 2)}')
-    print(f'\tAverage overbid: {round(np.mean(bids-makes), 2)} ± {round(np.std(bids-makes), 2)}')
+    print(f'\tAverage underbid: {round(np.mean(makes-bids), 2)} ± {round(np.std(makes-bids), 2)}')
+    print(f'\tAverage misbid magnitude: {round(np.mean(np.abs(makes - bids)), 2)} ± {round(np.std(np.abs(makes - bids)), 2)}')
+
     if non_zero_bid != 0:
         print(f'\tNonzero over/perfect/under rates: {round(len(overbids)/non_zero_bid*100)}% / {round(perfect/non_zero_bid*100)}% / {round(len(underbids)/non_zero_bid*100)}%')
     else:
         print(f'\tNonzero over/perfect/under rates: 0% / 0% / 0%')
 
     if zero_bid != 0:
-        print(f'\tZero success rate: {round(zero_made / zero_bid * 100)}%')
+        print(f'\tZero success rate: {round(zero_made / zero_bid * 100)}% ({zero_made}/{zero_bid})')
     else:
         print(f'\tZero success rate: n/a')
 
-    print(f'\tTeam bust rate: {round(sum(busts) / len(record) * 100)}%')
+    print(f'\tTeam bust rate: {round(sum(busts) / len(record) * 100, 2)}%')
 
     if PRINT_EXTRA:
         print('Extra Stats:')
-        print(f'\tAverage miss magnitude: {round(np.mean(np.abs(makes - bids)), 2)} ± {round(np.std(np.abs(makes - bids)), 2)}')
         if len(overbids) != 0:
-            print(f'\tAverage magnitude of overbids: {round(np.mean(overbids), 2)} ± {round(np.std(overbids), 2)}')
+            print(f'\tAverage magnitude of overbids: {round(np.mean(np.abs(overbids)), 2)} ± {round(np.std(overbids), 2)}')
         else:
             print(f'\tAverage magnitude of overbids: n/a')
         if len(underbids) != 0:
@@ -83,7 +84,7 @@ for name, record in records.items():
             print(f'\tAverage magnitude of underbids: n/a')
 
 
-        print(f'\tZero bid rate: {round(zero_bid / len(record) * 100)}%')
+        print(f'\tZero bid rate: {round(zero_bid / len(record) * 100, 2)}%')
 
 
     print('-'*30)
